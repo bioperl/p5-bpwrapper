e55486bd27dbe821f6ddc86c27ad675f252af989
rocky <rb@dustyfeet.com>
2016-07-01 18:23:02 -0400

    One more small tweak

---
+++ util/gh-test.sh
@@ -0,0 +1,11 @@
+if [[ $0 == ${BASH_SOURCE[0]} ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+gh_dir=$(pwd)
+short_name=$(basename $gh_dir)
+perl_list=$(perlbrew list | sed -e 's/[ *] perl-//')
+cd /tmp && rm -fr p5-bpwrapper; git clone $gh_dir
+cd /tmp/$short_name
+for perl in $perl_list ; do echo $perl; perlbrew use $perl && perl ./Build.PL >/dev/null && ./Build && make check | grep '^Result:'; echo '---'; done
+cd $gh_dir
