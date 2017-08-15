5c76629d73f7d966f09a9c36797382c7556a08d3
rocky <rb@dustyfeet.com>
2017-08-15 16:27:10 -0400

    gh-test.sh: add installdeps

--- util/gh-test.sh
+++ util/gh-test.sh
@@ -7,5 +7,5 @@ short_name=$(basename $gh_dir)
 perl_list=$(perlbrew list | sed -e 's/[ *] perl-//')
 cd /tmp && rm -fr p5-bpwrapper; git clone $gh_dir
 cd /tmp/$short_name
-for perl in $perl_list ; do echo $perl; perlbrew use $perl && perl ./Build.PL >/dev/null && ./Build && make check | grep '^Result:'; echo '---'; done
+for perl in $perl_list ; do echo $perl; perlbrew use $perl && perl ./Build.PL >/dev/null && ./Build installdeps && ./Build && make check | grep '^Result:'; echo '---'; done
 cd $gh_dir
